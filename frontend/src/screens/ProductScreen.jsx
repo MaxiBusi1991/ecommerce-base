f01503f354df5b97164722c0932bc96b99f009c2
ColmanNicolas <busimaximiliano@gmail.com>
2024-07-23 02:44:13 -0300

    se agrega spinner y mensaje product not found

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -9,6 +9,9 @@ import Badge from 'react-bootstrap/Badge';
 import Card from 'react-bootstrap/Card';
 import Button from "react-bootstrap/Button";
 import { Helmet } from 'react-helmet-async';
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { getError } from "../utils";
 
 
 const reducer = (state, action) => {
@@ -42,7 +45,7 @@ function ProductScreen() {
         const result = await axios.get(`/api/products/slug/${slug}`)
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data})
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload: err.message })
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) })
       }
 
     };
@@ -50,9 +53,9 @@ function ProductScreen() {
   },[slug])
 
   return loading ? (
-    <div>Loading...</div>
-  ) : error ? (
-    <div>{error}</div>
+    <LoadingBox/>
+  ): error ? (
+    <MessageBox variant='danger'>{error}</MessageBox>
   ) : (
     <div>
       <Row>
