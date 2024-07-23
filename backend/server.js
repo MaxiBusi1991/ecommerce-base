b8d60c4b3846f5f1f10b79cbb2816ca3fdc06f3c
ColmanNicolas <busimaximiliano@gmail.com>
2024-07-23 01:59:00 -0300

    creacion pagina descripcion del producto. Cambio de titulo usando helmet

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,22 @@ const app = express();
 const port = process.env.PORT || 5000
 
 app.get('/api/products', (req, res) => {
-  console.log(data)
   res.send(data.products)
+});
+
+app.get('/api/products/slug/:slug', (req, res) => {
+  // const product = data.products.find((x) => x.slug === req.params.slug)
+  // if(product) {
+  //   res.send(product)
+  // } else {
+  //   res.status(404).json({message: 'Product not found'})
+  // }
+  try {
+    const product = data.products.find((x) => x.slug === req.params.slug)
+    res.send(product)
+  } catch (error) {
+    res.status(404).json({message: 'Product not found'})
+  }
 });
 
 app.listen(port, () => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
           </Navbar>
         </header>
         <main>
-          <Container>
+          <Container className='mt-4'>
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen/>}/>
               <Route path="/" element={<HomeScreen/>}/>
