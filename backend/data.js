88d9215af5d09ec409305b1d42845bb34dd10368
ColmanNicolas <busimaximiliano@gmail.com>
2024-07-31 16:53:15 -0300

    subida de usuarios a MongoDB

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'maxi',
+      email: 'maxi@gmail.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true
+    },
+    {
+      name: 'nicole',
+      email: 'nicole@gmail.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false
+    }
+  ],
   products: [
     { 
       //_id: 1,
