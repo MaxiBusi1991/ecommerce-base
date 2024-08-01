7b33e587236e2737c18fef4fdcb924a0cd86c4c5
ColmanNicolas <busimaximiliano@gmail.com>
2024-08-01 17:08:26 -0300

    se crea pagina de formulario de datos para envio de producto

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,6 +16,8 @@ import { useContext } from 'react';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
+
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -24,6 +26,7 @@ function App() {
   const signoutHandler = () => {
     ctxDispatch({type: 'USER_SIGNOUT'});
     localStorage.removeItem('userInfo');
+    localStorage.removeItem('shippingAddress');
   }
   return (
     <BrowserRouter>
@@ -76,6 +79,7 @@ function App() {
               <Route path="/product/:slug" element={<ProductScreen/>}/>
               <Route path="/cart" element={<CartScreen/>}/>
               <Route path="/signin" element={<SigninScreen/>}/>
+              <Route path="/shipping" element={<ShippingAddressScreen/>}/>
               <Route path="/" element={<HomeScreen/>}/>
             </Routes>
           </Container>
